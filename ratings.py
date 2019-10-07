79e7347a96085d314f5ef4e4c7515a086273e4e8
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-07 12:50:31 -0700

    WIP: wrote add_rating and sort_print functions

--- ratings.py
+++ ratings.py
@@ -1,4 +1,15 @@
 """Restaurant rating lister."""
+def add_rating():
+
+    restaraunt_name = input("Name of restaraunt: > ")
+    restaraunt_rating = input("Rating: >")
+
+    restaraunt_name = restaraunt_name.title()
+
+    return (restaraunt_name, restaraunt_rating)
+
+
+
 def read_ratings(text_file):
 
     text_file = open(text_file)
@@ -9,16 +20,30 @@ def read_ratings(text_file):
         restaraunt_rating = line.split(":")
         rating_dict[restaraunt_rating[0]] = restaraunt_rating[1]
 
-    # print(rating_dict)
+    def sort_print_resto():
+        restaraunts = sorted((rating_dict.keys()))
+    # print(restaraunts)
 
-    restaraunts = sorted((rating_dict.keys()))
-    print(restaraunts)
+        for restaraunt in restaraunts:
+            print(f'{restaraunt} is rated at {rating_dict[restaraunt]}')
 
-    for restaraunt in restaraunts:
-        print(f'{restaraunt} is rated at {rating_dict[restaraunt]}')
+    sort_print_resto()
+
+    want_to_add = input("Want to add a restaraunt rating? (Y/N) > ")
+
+    if want_to_add == 'Y':
+        restaraunt_name, restaraunt_rating = add_rating()
+        rating_dict[restaraunt_name] = restaraunt_rating
+    elif want_to_add != 'N':
+        print("That's not a valid answer")
+
+
+    sort_print_resto()
 
 
     text_file.close()
+
+
 
 read_ratings("scores.txt")
 # put your code here
