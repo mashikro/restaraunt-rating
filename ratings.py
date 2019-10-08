d7fcec24dff40a49e73f4fb193cab884cf82445f
vagrant <vagrant@vagrant.vm>
2019-10-08 00:27:05 +0000

    Implemented user choice flow feature

--- ratings.py
+++ ratings.py
@@ -9,6 +9,20 @@ def add_rating():
     return (restaraunt_name, restaraunt_rating)
 
 
+def sort_print_resto(rating_dict):
+    restaraunts = sorted((rating_dict.keys()))
+    # print(restaraunts)
+
+    for restaraunt in restaraunts:
+        print(f'{restaraunt} is rated at {rating_dict[restaraunt]}')
+
+
+def user_choice():
+    print("What do you want to do? ")
+    print("You can A. See the restaraunt list. B. Add your own rating. C. Quit")
+    decision = input("Your choice: > ")
+
+    return decision.upper()
 
 def read_ratings(text_file):
 
@@ -20,30 +34,25 @@ def read_ratings(text_file):
         restaraunt_rating = line.split(":")
         rating_dict[restaraunt_rating[0]] = restaraunt_rating[1]
 
-    def sort_print_resto():
-        restaraunts = sorted((rating_dict.keys()))
-    # print(restaraunts)
+    decision = user_choice()
+ 
+    while decision != 'C':
 
-        for restaraunt in restaraunts:
-            print(f'{restaraunt} is rated at {rating_dict[restaraunt]}')
-
-    sort_print_resto()
-
-    want_to_add = input("Want to add a restaraunt rating? (Y/N) > ")
-
-    if want_to_add == 'Y':
-        restaraunt_name, restaraunt_rating = add_rating()
-        rating_dict[restaraunt_name] = restaraunt_rating
-    elif want_to_add != 'N':
-        print("That's not a valid answer")
-
-
-    sort_print_resto()
-
+        if decision == 'A':
+            sort_print_resto(rating_dict)
+            decision = user_choice()
+        elif  decision == 'B':
+            restaraunt_name, restaraunt_rating = add_rating()
+            rating_dict[restaraunt_name] = restaraunt_rating
+            decision = user_choice()
+        # elif decision == 'C':
+        #     print('Goodbye')
+             
+        else:
+            print('That was not a valid answer')
+    
 
     text_file.close()
 
 
-
 read_ratings("scores.txt")
-# put your code here
